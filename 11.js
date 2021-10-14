c01b019331601e01eb3f4b34b687d85e070d27a0
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-14 17:31:33 +0530

    null undefined bigint

---
+++ 11.js
@@ -0,0 +1,19 @@
+//undefined
+let names;
+console.log(names);
+var nano;
+console.log(nano);
+// const t;
+// console.log(t);
+
+//null
+
+let myVar = null;
+console.log(typeof myVar);
+//it is an error
+
+//Bigint
+let num = BigInt(11);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(num);
+//big int cannot be addede with other
