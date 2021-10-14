297c867ca0a5f9de9f77c58cf972cb448fb0299d
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-14 17:07:26 +0530

    operator and conversions

---
+++ 08.js
@@ -0,0 +1,31 @@
+//typeof operator
+// primitive data types
+//string "harshit"
+//number 1,2,3,4,5,6,7,8,9,10
+//boolean 
+//null
+//BigInt 
+//Symbol
+let age = 22
+let name = "ashu"
+console.log(typeof name);
+console.log(typeof(age));
+
+//convert number to String
+console.log(typeof (age + " "));
+
+//convert string to number
+let myString = "34"
+console.log(typeof myString);
+let mySttring = +"23";
+console.log(typeof mySttring);
+console.log(mySttring);
+
+let age1 = 22
+age1 = String(age1);
+console.log(typeof age1);
+console.log(age1);
+let str = "22"
+str = Number(str);
+console.log(typeof str)
+console.log(str);
