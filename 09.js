a118a973d95cdafde057a1f565299e3cf29e2607
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-14 17:10:41 +0530

    string concat

---
+++ 09.js
@@ -0,0 +1,10 @@
+//string concatenation
+
+let string1 = "harshit";
+let string2 = " vasistha";
+let fullName = string1 + " " + string2;
+console.log(fullName);
+
+let num1 = "16"
+let num2 = "16"
+console.log(num1 + " " + num2)
