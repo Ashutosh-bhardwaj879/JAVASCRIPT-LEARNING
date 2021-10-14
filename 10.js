29d99e66bf1a74aa3fa2d838fa811092c48b31fd
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-14 17:20:03 +0530

    template string

---
+++ 10.js
@@ -0,0 +1,10 @@
+// template string
+
+let age = 22
+let name = "ab"
+
+let me = "my name is " + name + " and my age is " + age;
+console.log(me);
+
+let me2 = `my name is ${name} and my age is ${age}`;
+console.log(me2);
