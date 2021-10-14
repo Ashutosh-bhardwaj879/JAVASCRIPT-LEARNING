9fb706117f95bf14e879241fb08e0b8c7c883b19
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-14 17:38:44 +0530

    == vs ===

---
+++ 12.js
@@ -0,0 +1,19 @@
+//boolean & comparison operator
+let num1 = 7;
+let num2 = 7;
+
+console.log(num1 <= num2);
+
+
+// == vs ===
+let num3 = "7"
+console.log(num1 == num3)
+//== checks value but not datatype
+console.log(num1 === num3)
+// === checks value as well as datatype
+
+// != vs !==
+//!= doesnt check datatype but checks value only
+console.log(num1 != num2);
+console.log(num1 !== num3);
+console.log(num1 != num3);
